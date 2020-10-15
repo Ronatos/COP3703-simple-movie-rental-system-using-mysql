8c7a80b0f0342335d9923513e870e3b07e97a979
Ronatos <alexander.l.perez@outlook.com>
2020-10-15 13:57:22 -0400

    further eclipse project updates

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,10 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-
-	public static Connection getConnection() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Establishing database connection...");
-        System.out.print("Username: ");
-        String username = scanner.nextLine();
-        System.out.print("Password: ");
-        String password = scanner.nextLine();
-        scanner.close();
-        
-        Connection conn = null;
-		String url = "jdbc:mysql://cisvm-winsrv-mysql1.unfcsd.unf.edu:3308/group2";
-        try {
-            conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Database connection established.");
-        } catch (SQLException e) {
-        	System.out.println("Database connection failed to establish. Aborting...");
-            e.printStackTrace();
-        }
-        return conn;
-	}
-	
-	public static void query() {
-		try {
-            Connection conn = getConnection();
-			Statement st = conn.createStatement(
-				ResultSet.TYPE_SCROLL_INSENSITIVE,
-				ResultSet.CONCUR_READ_ONLY);
-			ResultSet rs = st.executeQuery("SELECT * FROM Customers");
-			while (rs.next()) {
-				System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public Main() {
-		getConnection();
-	}
 	
 	public static void main(String[] args) {
-		query();
+		Scanner scanner = new scanner(System.in);
+		DatabaseBridge connection = new DatabaseBridge()
 	}
+	
 }
