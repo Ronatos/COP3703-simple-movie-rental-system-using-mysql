83cfa20f917473fb5af1f058ee8344642921a14b
Ronatos <alexander.l.perez@outlook.com>
2020-10-15 19:32:18 -0400

    minor updates and git example

--- src/Query.java
+++ src/Query.java
@@ -3,6 +3,14 @@ import java.sql.*;
 public class Query {
 
 	// This method is currently being tested, and should be adjusted to verify username and password eventually
+	/**
+	 * isExistingCustomer is currently being tested, and should be adjusted to verify username and password eventually.
+	 * @param connection The connection to the database.
+	 * @param FirstName
+	 * @param LastName
+	 * @return
+	 * @throws SQLException
+	 */
 	public static boolean isExistingCustomer(Connection connection, String FirstName, String LastName) throws SQLException {
 		// Set up query environment
 		Statement statement = null;
